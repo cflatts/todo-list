1cca3c8f50660ac00a6fe3f1451d805cad49e3dd
Caitlin Flattery <caitlinflattery@Caitlins-MacBook-Air.local>
2016-07-04 15:21:29 -0500

    set up lifecycle events

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -1,6 +1,34 @@
 import React from 'react'
 
+var ListView = React.createClass ({
 
+    getInitialState: function () {
+        return {
+            listColl: this.props.listColl
+            }
+    },
+
+    componentWillMount: function () {
+        console.log('mounting')
+        this.props.listColl.on('update', () => {
+            this.setState({
+                listColl: this.state.listColl
+            })
+        })
+    },
+
+    _addTask : function(taskInfo) {
+        this.props.listColl.add({
+            task: taskInfo
+        })
+    },
+
+    render: function() {
+        return(
+            <div>Hi</div>
+            )
+    }
+})
 
 
 export default ListView
